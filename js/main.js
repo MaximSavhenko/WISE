2acf91769c79feca953f80fba1bc3c705375664c
maximsavhenko <uspmsm@gmail.com>
2020-06-21 21:36:45 +0300

    init project

---
+++ js/main.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+	$("a").click(function () {
+		elementClick = $(this).attr("href");
+		destination = $(elementClick).offset().top;
+		$("body,html").animate({scrollTop: destination }, 1200);
+	});
+});
+
