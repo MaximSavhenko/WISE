e528c43c4782db2964b22961dadc7ea110e5e070
maximsavhenko <uspmsm@gmail.com>
2020-06-24 23:27:55 +0300

    add slider

--- js/main.js
+++ js/main.js
@@ -8,12 +8,9 @@ $(document).ready(function(){
 
 
 
-// $('.projects__main-wrap').slick({
-// 	infinite: true,
-// 	slidesToShow: 3,
-// 	slidesToScroll: 3,
-// 	dots: true
-// });
+$('.projects__main-wrap').slick({
+	
+});
 
 
   
