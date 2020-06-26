558ee370a064c065134a87bcd8c1cf422d651163
MaximSavhenko <uspmsm@gmail.com>
2020-06-26 13:21:33 +0300

    fix after reviews

--- js/main.js
+++ js/main.js
@@ -5,15 +5,42 @@ $(document).ready(function(){
 		$("body,html").animate({scrollTop: destination }, 800);
 	});
 	
-	$('.projects__main-wrap , .blogs__main-wrap').slick({
+	$('.projects__main-wrap').slick({
 		arrows: true,
 		dots: true
 	});
 	
-	// $('.blogs__main-wrap').slick({
-	// 	arrows: true,
-	// 	dots: true
-	// });
+	$('.blogs__main-wrap').slick({
+		arrows: true,
+		dots: true,
+		slidesToShow: 3,
+		slidesToScroll: 3,
+		responsive: [
+			{
+			  breakpoint: 1024,
+			  settings: {
+				slidesToShow: 2,
+				slidesToScroll: 2,
+				dots: true
+			  }
+			},
+			{
+			  breakpoint: 600,
+			  settings: {
+				slidesToShow: 1,
+				slidesToScroll: 1,
+				dots: true
+			  }
+			},
+			{
+			  breakpoint: 480,
+			  settings: {
+				slidesToShow: 1,
+				slidesToScroll: 1
+			  }
+			}
+		]
+	});
 });
 
   
