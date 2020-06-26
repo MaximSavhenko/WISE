b88d53aa63982e451c356909006e491862ca7cfc
MaximSavhenko <uspmsm@gmail.com>
2020-06-26 18:05:07 +0300

    add burger menu

--- js/main.js
+++ js/main.js
@@ -63,6 +63,18 @@ function openbox(box){
 	}
 }
 
+function toggleMobileMenu() {
+	var btnMenu = document.querySelector('.header__btn-menu'),
+		navWrap = document.querySelector('.header__wrap-nav');
+  
+	btnMenu.onclick = function () {
+	  document.body.classList.toggle('overflow');
+	  btnMenu.classList.toggle('active');
+	  navWrap.classList.toggle('active');
+	};
+  };
+  toggleMobileMenu();
+
 
   
 	
